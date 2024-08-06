24076ed9095f76f799451d726a278ccc5533b2f0
pvy-it-tdh <phucvy107@gmail.com>
2024-08-06 14:36:03 +0700

    update

---
+++ src/main/java/com/example/phucvy/MicroseviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.phucvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroseviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroseviceApplication.class, args);
+	}
+
+}
